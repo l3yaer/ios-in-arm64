743bd9d75822a883da83df3ec9e98b2df4a59b75
Yago Teixeira <yago.teixeira@sicoob.com.br>
2022-11-17 10:28:38 -0300

    Initial Commit

---
+++ iOSInARMv7/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iOSInARMv7
+//
+//  Created by Yago Teixeira on 17/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOSInARMv7/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iOSInARMv7
+//
+//  Created by Yago Teixeira on 17/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
